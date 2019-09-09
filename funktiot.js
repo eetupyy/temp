43d1d7c4b2aba27cef99af139c3196541dd68c0a
Eetu Pyy <eetu.pyy@itvelhot.local>
2019-09-09 10:36:26 +0300

    Eka commit

---
+++ funktiot.js
@@ -0,0 +1,20 @@
+// var celtofah = 
+// var fahtocel = x - 32 / 1.8
+
+function temp(){
+
+}
+
+
+function celToFah(c){
+    return c * 1.8 + 32;
+}
+
+function fahToCel(f){
+    return f - 32 / 1.8
+}
+
+function desi1() {
+
+}
+<button onclick="temp()">Laske</button>
